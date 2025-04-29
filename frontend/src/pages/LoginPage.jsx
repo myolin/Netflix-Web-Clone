2ef6629ce14c97196ea841f11a3dfbea2694df09
myolin <mzlmyo@gmail.com>
2025-04-29 00:45:03 -0500

    auth loading states added, right side scroll bar color changed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,7 +6,7 @@ function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   
-  const { login } = useAuthStore();
+  const { login, isLoggingIn } = useAuthStore();
 
   const handleLogIn = (e) => {
     e.preventDefault(); // Prevent from refreshing the page upon form submit
@@ -56,8 +56,9 @@ function LoginPage() {
 
             <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
                 hover:bg-red-700'
+                disabled={isLoggingIn}
             >
-              Login
+              {isLoggingIn ? "Loading..." : "Login"}
             </button>
           </form>
           <div className='text-center text-gray-400'>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -10,7 +10,7 @@ function SignUpPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const {signup} = useAuthStore();
+  const {signup, isSigningUp } = useAuthStore();
 
   const handleSignUp = (e) => {
     e.preventDefault(); // Prevent from refreshing the page upon form submit
@@ -74,8 +74,9 @@ function SignUpPage() {
 
           <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md
             hover:bg-red-700'
+            disabled={isSigningUp}
           >
-            Sign Up
+            {isSigningUp ? "Loading..." : "Sign Up"}
           </button>
           </form>
           <div className='text-center text-gray-400'>
