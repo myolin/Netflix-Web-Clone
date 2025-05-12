d12fc7e794a7f54b636772569f43ce511f47e721
myolin <mzlmyo@gmail.com>
2025-05-12 14:18:48 -0500

    Login Page wrong text fixed

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ function LoginPage() {
 
       <div className="flex justify-center items-center mt-20 mx-3">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
-          <h1 className="text-center text-white text-2xl font-bold mb-4">Sign up</h1>
+          <h1 className="text-center text-white text-2xl font-bold mb-4">Login</h1>
           <form className="space-y-4" onSubmit={handleLogIn}>
 
             <div>
